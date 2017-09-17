c24268473e9aed2413f48d5b195d327080d650d2
Cory Shaw <cory.shaw.dev@gmail.com>
2017-09-17 13:33:01 -0400

    fix(server): Fix missing vars and add package dependencies

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 //  OpenShift sample Node application
-var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
+var express = require('express');
+var app     = express();
+var fs      = require('fs');
+var pkg     = require(process.cwd() + "/package.json");
+var path    = require("path");
+var request = require("request");
     
 Object.assign=require('object-assign')
-
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
@@ -75,7 +75,9 @@ initDb(function(err){
   console.log('Error connecting to Mongo. Message:\n'+err);
 });
 
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
+app.listen(port, ip, function() {
+  console.log('%s: Node server started on http://%s:%s ...',
+              Date(Date.now() ), ip, port);
+});
 
 module.exports = app ;
